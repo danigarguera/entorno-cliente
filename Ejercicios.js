0a5ff8ec7efe5f333b950831f96cc85e91929759
danigarguera <danielguillenguerrero@gmail.com>
2024-10-04 14:08:25 +0200

    añadidos nuevos ejercicios

--- Ejercicios.js
+++ Ejercicios.js
@@ -8,8 +8,8 @@ console.log("Nombre",nombre);
 console.log("edad",edad);
 console.log("Me gusta programar",megustaProgramar);
 //we give values to the variables
-let n1=12;
-let n2=3;
+let nun1=12;
+let nun2=3;
 //we make de addition
 let res=(n1+n2);
 //show the result
@@ -25,10 +25,10 @@ let numero2=Number(numero);
 //sow to the user the type of variable
 console.log(typeof(numero2));
 //make the different operations
-let resta=(n1-n2);
-let suma=(n1+n2);
-let division=(n1/n2);
-let producto=(n1*n2);
+let resta=(nun1-nun2);
+let suma=(nun1+nun2);
+let division=(nun1/nun2);
+let producto=(nun1*nun2);
 // show the results
 console.log("suma",suma);
 console.log("resta",resta);
@@ -127,4 +127,71 @@ switch(operador){
         console.log("El resultado es: ", resultado);
 
     }
+}
+
+// Ask for the phrase to the user
+let frase = readlineSync.question('Introduce una frase: ');
+
+// Use split to divide the phrase in separate words
+let palabras = frase.trim().split(/\s+/); // trim() delete the spaces of the begining and the end \s+ split the phrase in different spaces
+
+// Show the number of words
+console.log("La frase tiene ", palabras.length , "palabras.");
+
+
+// Ask for the phrase to the user
+let frase2 = readlineSync.question('Introduce una frase: ');
+
+// Use split to divide the phrase in separate words
+let palabras2 = frase.trim().split(/\s+/); // trim() delete the spaces of the begining and the end \s+ split the phrase in different spaces
+//Run the array length backwards printing each word of the array
+for (let i= palabras.length; i>=0;i--){
+    console.log(palabras[i]);
+}
+
+// Ask for the to the numbres 
+let n1 = Number(readlineSync.question('numero 1: ')); //use numbers to convert the variables to numbers
+let n2 = Number(readlineSync.question('numero 2: '));
+let n3 = Number(readlineSync.question('numero 3: '));
+//make the conditions for the different events
+if (n1<=n2 && n2<n3){
+    console.log(n3," es el numero mayor")
+} else if(n1>n2 && n2>=n3){
+    console.log(n1," es el numero mayor")
+} else if (n1<=n3 && n3<n2){
+    console.log(n2," es el numero mayor")
+
+} else if (n1==n2 && n2==n3){
+    console.log("Todos los numeros tienen el mismo valor")
+
+}else if (n1<n3 && n3==n2){
+    console.log(n2," es el numero mayor")
+
+}else if (n1==n3 && n3>n2){
+    console.log(n1," es el numero mayor")
+
+}else if (n1==n2 && n2>n3){
+    console.log(n2," es el numero mayor")
+
+}
+
+//we ask the user for introduce a number
+let np = readlineSync.question('introduce un numero: ');
+//we use the Wilson Theoreme to calculate if the number is a prime number
+let rp=1;
+// we deduct 1 to the number that we want to know if is a prime number or not
+let nnp=(np-1);
+//calculate the factorial of this number
+while (nnp>1){
+    rp=nnp*rp
+    nnp--;
+
+}
+//Add 1 to the result
+rp=(rp+1);
+//If the reminder of divide this number by the number that we want to figure out if is a prime number is 0 then is a prime number.
+if(rp%np==0){
+    console.log("El numero es primo")
+}else{
+    console.log("El numero no es primo")
 }
